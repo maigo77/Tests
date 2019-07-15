933c56e3aa1bc0dd0d29387bab5f521e97bf53e3
maigo77 <38216218+maigo77@users.noreply.github.com>
2019-07-15 17:55:15 -0300

    Add files via upload

--- crud_com_ajax/index.php
+++ crud_com_ajax/index.php
@@ -4,24 +4,34 @@
         <meta charset="UTF-8" http-equiv="Content-type" content="text/html">
         <title>Formulário com Ajax e PHP</title>
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
         <script src="https://kit.fontawesome.com/a3361d289b.js"></script>
+        <script src="js/modal.js"></script>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
         <link rel="stylesheet" href="css/estilo.css">
     </head>
     <body>
-        <a href="#janela1" rel="modal">Clique aqui para cadastrar um novo usuário</a>
-        <br><br>
         <div id="table">
-            <h2>Usuários cadastrados</h2>
-            <table class="table table-striped table-bordered table-condensed table-hover">
-                <tr>
-                    <td>Id</td>
-                    <td>Nome</td>
-                    <td>Email</td>
-                    <td>Status</td>
-                    <td>Senha</td>
-                    <td>Ações</td>
-                </tr>
+            <div id="divMaior">
+                <div id="divMenor">
+                    <h2>
+                        Usuários cadastrados
+                        <i class="fa fa-users no-pointer" aria-hidden="true"></i>
+                    </h2>
+                </div>
+            </div>
+            
+            <table class="text-center table table-striped table-bordered table-responsive table-hover">
+                <thead>
+                    <tr class="bg-primary table-titles">
+                        <td>Id</td>
+                        <td>Nome</td>
+                        <td>Email</td>
+                        <td>Status</td>
+                        <td>Senha</td>
+                        <td>Ações</td>
+                    </tr>
+                </thead>
+                <tbody>
                 <?
                 include 'conexao.php';
 
@@ -35,30 +45,47 @@
                     $status = $row['STATUS'];
                     $senha = $row['SENHA'];
 
-                    echo "<tr>
-                    <td>$id </td>
-                    <td>$nome</td>
-                    <td>$email</td>
-                    <td>$status</td>
-                    <td>$senha</td>
-                    <td><span class='excluir' id_registro='$id'>Excluir</span> | <a href='#janela2' class='edita' id_registro='$id' rel='modal'>Editar</a> | <span class='status' status='$status' id_registro='$id'>Alterar Status</span> </td>
-
-                    </tr>";
+                    echo "
+                    <tr>
+                        <td>$id </td>
+                        <td>$nome</td>
+                        <td>$email</td>
+                        <td>$status</td>
+                        <td>$senha</td>
+                        <td>
+                            <i title='Excluir' class='excluir fa fa-minus-circle' aria-hidden='true' id_registro='$id'></i> 
+                            | 
+                            <a title='Editar' href='#janela2' class='edita' id_registro='$id' rel='modal'>
+                                <i class='fa fa-pencil-square' aria-hidden='true'></i>
+                            </a> 
+                            | 
+                            <i title='Alterar Status' class='fa fa-wrench status' status='$status' id_registro='$id' aria-hidden='true'></i> 
+                        </td>
+                    </tr>";                                                                 
                 }
                 /* onclick=\"alert('".$id."');\" */
                 ?>
+                    </tbody>
             </table>
 
+            <hr>
+
+            <div id='add-user'>
+                <a href="#janela1" rel="modal">
+                    Clique aqui para cadastrar um novo usuário
+                    <i class="fa fa-user-plus" aria-hidden="true"></i>
+                </a>
+            </div>
 
             <div class="window" id="janela1">
                 <i class="fa fa-window-close fechar" aria-hidden="true"></i>
                 <h4>Cadastro de Usuário</h4>
                 <form id="cadUsuario" action="" method="post">
-                    <label>Nome:</label><input type="text" name="nome" id="nome">
-                    <label>Email:</label><input type="text" name="email" id="email">
-                    <label>Senha:</label><input type="text" name="senha" id="senha">
+                    <label>Nome:</label><input type="text" name="nome" id="nome" required>
+                    <label>Email:</label><input type="email" name="email" id="email" required>
+                    <label>Senha:</label><input type="text" name="senha" id="senha" required>
                     <br><br>
-                    <input type="button" value="Cadastrar" id="salvar">
+                    <input class="button-modal" type="button" value="Cadastrar" id="salvar">
                 </form>
             </div>
 
@@ -66,11 +93,11 @@
                 <i class="fa fa-window-close fechar" aria-hidden="true"></i>
                 <h4>Editar Dados</h4>
                 <form id="edit" action="" method="post">
-                    <label>Nome:</label><input type="text" name="nome" id="nome">
-                    <label>Email:</label><input type="text" name="email" id="email">
-                    <label>Senha:</label><input type="text" name="senha" id="senha">
+                    <label>Nome:</label><input type="text" name="nome" id="nome" required>
+                    <label>Email:</label><input type="text" name="email" id="email" required>
+                    <label>Senha:</label><input type="text" name="senha" id="senha" required>
                     <br><br>
-                    <input type="button" value="Salvar" id="editar">
+                    <input class="button-modal" type="button" value="Salvar" id="editar">
                 </form>
             </div>
 
@@ -162,36 +189,6 @@
             });
 
             return false;
-        });
-
-        $("a[rel=modal]").click(function(ev){
-            ev.preventDefault();
-
-            var id = $(this).attr("href");
-
-            var alturaTela = $(document).height();
-            var larguraTela = $(document).width();
-
-            $('#mascara').css({'width': larguraTela, 'height': alturaTela});
-            $('#mascara').fadeIn(1000);
-            $('#mascara').fadeTo("slow", 0.8);
-
-            var left = ($(window).width() / 2) - ($(id).width() / 2);
-            var top = ($(window).height() / 2) - ($(id).height() / 2);
-
-            $(id).css({'top': top, 'left': left});
-            $(id).show();
-        });
-
-        $("#mascara").click(function(){
-            $(this).hide();
-            $(".window").hide();
-        });
-
-        $(".fechar").click(function(ev){
-            ev.preventDefault();
-            $("#mascara").hide();
-            $(".window").hide();
         });
 
     });
